75801cda128b15da7ecab05178f644608de74a59
Joana Maria <joanamariacardoso1601@gmail.com>
2024-04-21 23:23:04 +0100

    performance: applying useMemo

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,4 +1,5 @@
 import { TransactionsContext } from '@/contexts/TransactionsContext';
+import { useMemo } from 'react';
 
 import { useContextSelector } from 'use-context-selector';
 
@@ -7,23 +8,25 @@ export function useSummary() {
     return context.transactions;
   });
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
+  const summary = useMemo(() => {
+    transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price;
+          acc.total += transaction.price;
+        } else {
+          acc.outcome += transaction.price;
+          acc.total -= transaction.price;
+        }
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0
       }
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0
-    }
-  );
+    );
+  }, [transactions]);
 
   return summary;
 }
